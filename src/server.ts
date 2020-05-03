8e88c1ba9c3ab556153ef86a194cc0f24b536728
Abhishek Bhaumik <bhaumik.abhishek@gmail.com>
2020-05-03 13:50:47 -0500

    Declared Typescript variable in server.ts

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
 
-  app.get("/filteredimage/", async (req, res) => {
+  app.get("/filteredimage/", async (req : REQUEST, res: RESPONSE) => {
     let { image_url } = req.query;
 
     if (!image_url){
